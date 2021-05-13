8e0abcc756f9cf4c14916d634abc10990679479e
unknown <muneebali500@gmail.com>
2021-05-13 11:07:33 +0500

    8.using/6.intro

--- server.js
+++ server.js
@@ -1,6 +1,53 @@
+import mongoose from 'mongoose';
 import app from './app.js';
 
 // console.log(process.env);
+
+const DB = process.env.DATABASE.replace(
+  `<PASSWORD>`,
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => console.log(`DB connection successful`));
+
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, `A tour must have a name`],
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    required: [true, `A tour must have a price`],
+  },
+});
+
+const Tour = mongoose.model(`Tour`, tourSchema);
+
+const testTour = new Tour({
+  name: `The jump jaker`,
+  price: 4500,
+});
+
+testTour
+  .save()
+  .then((doc) => {
+    console.log(doc);
+  })
+  .catch((err) => {
+    console.log(`Error 🔥:`, err);
+  });
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
