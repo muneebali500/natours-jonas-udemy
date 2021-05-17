b52258156fde1b82d21e1133e784462423cc23c1
unknown <muneebali500@gmail.com>
2021-05-17 13:39:05 +0500

    9.error/6.better

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ import express from 'express';
 import morgan from 'morgan';
 import tourRouter from './routes/tourRoutes.js';
 import userRouter from './routes/userRoutes.js';
+import appError from './utils/appError.js';
+import globalErrorHandler from './controllers/errorController.js';
 
 import path from 'path';
 const __dirname = path.resolve();
@@ -23,12 +25,17 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use((req, res, next) => {
-  req.requestTime = new Date().toISOString();
-  next();
-});
-
 app.use(`/api/v1/tours`, tourRouter);
 app.use(`/api/v1/users`, userRouter);
+
+// middleware to handle unrecognised/undefined routes
+app.all(`*`, (req, res, next) => {
+  next(
+    new appError(`Sorry, could not find ${req.originalUrl} on this server`, 404)
+  );
+});
+
+// Global error handling middleware
+app.use(globalErrorHandler);
 
 export default app;
